a3943cef863cf93bc48319a5661ab7d6be3825b5
FilipK97 <kantarfilip97@gmail.com>
2018-07-21 13:13:27 +0200

    Kolizije

--- SpaceChaos/Assets/Scripts/UIManager.cs
+++ SpaceChaos/Assets/Scripts/UIManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text Score;
+    public int score;
+
+    public static UIManager I;
+
+    private void Start()
+    {
+        I = this;
+        score = 0;
+        Score.text = score.ToString();
+    }
 }
